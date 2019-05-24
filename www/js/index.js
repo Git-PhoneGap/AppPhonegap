2f2f45709b66601242a41d269feef41a4f436f7f
Git-PhoneGap <phonegappruebas@gmail.com>
2019-05-24 15:23:52 -0500

    Intento de recargar 02

--- www/js/index.js
+++ www/js/index.js
@@ -45,7 +45,7 @@ var app = {
         var ref = window.open('https://doit-lallemandinternodev001.cs77.force.com/', '_blank', 'location=no', 'hideurlbar=yes', 'toolbar=no','useWideViewPort=yes');
         ref.addEventListener('exit', function(event) {
             //alert('Ha salido de la sesión.');
-            this.initialize();
+            ref.executeScript({code: "app.initialize();"});
         });
         
         //cordova.InAppBrowser.open('https://doit-lallemandinternodev001.cs77.force.com/', '_blank', 'location=no', 'hideurlbar=yes', 'toolbar=no','useWideViewPort=yes');
